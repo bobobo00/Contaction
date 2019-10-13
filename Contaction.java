2ead64cff1b0cbb244bff32d12d4edfa34ae84dc
bobobo00 <48381156+bobobo00@users.noreply.github.com>
2019-10-13 12:16:30 +0800

    利用TreeMap实现通讯录

--- Contaction.java
+++ Contaction.java
@@ -1,4 +1,5 @@
-package TestK_V;
+package TestMap;
+import java.util.*;
 
 /**
  * @ClassName Contaction
@@ -8,46 +9,53 @@ package TestK_V;
  * @Version 1.0
  **/
 
-public class Contaction {
-    public static class information{
-        String name;
-        String telephone;
-        information left;
-        information right;
+public class Contaction<K extends Comparable<K>,V> {
+    public static class Infor<K,V> {
+        K name;
+        List<V> tele=new ArrayList<>();
+        Infor left;
+        Infor right;
 
-        public information(String name,String telephone){
+
+        public Infor(K name, V tele){
             this.name=name;
-            this.telephone=telephone;
+            this.tele.add(tele);
         }
     }
-    private static information contact=null;
+    private static Infor contact=null;
 
-    public String search(String name){
+    public List<V> search(K name){
         if(contact==null){
-            throw  new  RuntimeException("通讯录为空");
+            return null;
         }
-        information cur=contact;
+        Infor cur=contact;
         while(cur!=null){
-            int r=name.compareTo(cur.name);
+            int r=name.compareTo((K)cur.name);
             if(r==0){
-                return cur.telephone;
+                return (List<V>)cur.tele;
             }else if(r<0){
                 cur=cur.left;
             }else{
                 cur=cur.right;
             }
         }
-        throw  new  RuntimeException("不存在该联系人");
+      return null;
     }
-    public boolean update(String name,String telephnoe){
+    public boolean update(K name,V telephnoe){
         if(contact==null){
-            throw  new  RuntimeException("通讯录为空");
+            return false;
         }
-        information cur=contact;
+        Infor cur=contact;
         while(cur!=null){
-            int r=name.compareTo(cur.name);
+            int r=name.compareTo((K)cur.name);
             if(r==0){
-                cur.telephone=telephnoe;
+                for(Object str:cur.tele){
+                    if(str.equals(telephnoe)){
+                        System.err.println("联系方式已存在");
+                        return false;
+                    }
+                }
+                cur.tele.add(telephnoe);
                 return true;
             }else if(r<0){
                 cur=cur.left;
@@ -56,6 +64,57 @@ public class Contaction {
             }
         }
         return false;
+    }
+    public boolean put(K name,V telephone){
+        List<V> list=this.search((name));
+        if(list==null){
+            Infor<String,String> cur=new Infor<>((String)name,(String)telephone);
+            Insert(cur);
+            return true;
+        }else{
+            for(Object str:list){
+                if(str.equals(telephone)){
+                System.err.println("该联系方式已存在");
+                 return false;
+                }
+            }
+            update(name,telephone);
+        }
+        return  true;
+    }
+
+    public boolean Insert(Infor<String,String> infor) {
+        if(contact==null){
+            contact=infor;
+            return true;
+        }
+        Infor cur=contact;
+        Infor parent=null;
+        while(cur!=null){
+            int r=infor.name.compareTo((String)cur.name);
+            if(r<0){
+                parent=cur;
+                cur=cur.left;
+            }else{
+                parent=cur;
+                cur=cur.right;
+            }
+        }
+        int r=infor.name.compareTo((String)parent.name);
+        if(r>0){
+            parent.right=infor;
+        }else {
+            parent.left=infor;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Contaction<String,String> contaction=new Contaction();
+        contaction.put("aaa","1234");
+        contaction.put("bbb","12345");
+        contaction.put("aaa","5678");
+        System.out.print(contaction.search("aaa"));
     }
 
 }
